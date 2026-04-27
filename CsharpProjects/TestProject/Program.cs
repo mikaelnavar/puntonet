5970bb89ed759fe71447ca4f68fa556c42e2fcfd
mikaelnavar <89931345+mikaelnavar@users.noreply.github.com>
2026-04-27 21:28:55 +0000

    if statement

---
+++ CsharpProjects/TestProject/Program.cs
@@ -0,0 +1,36 @@
+﻿Random dice = new Random();
+
+int roll1 = dice.Next(1, 7);
+int roll2 = dice.Next(1, 7);
+int roll3 = dice.Next(1, 7);
+
+int total = roll1 + roll2 + roll3;
+
+Console.WriteLine($"Dice roll: {roll1} + {roll2} + {roll3} = {total}");
+
+if ((roll1 == roll2) || (roll2 == roll3) || (roll1 == roll3))
+{
+    Console.WriteLine("You rolled doubles! +2 bonus to total!");
+    total += 2;
+}
+if (total > 14)
+{
+    Console.WriteLine("You win");
+}
+
+if (total < 15)
+{
+    Console.WriteLine("Sorry, you loose");
+}
+
+
+/*string message = "The quick brown fox jumps over the lazy dog.";
+bool result = message.Contains("dog");
+Console.WriteLine(result);
+
+if (message.Contains("fox"))
+{
+    Console.WriteLine("What does the fox say?");
+
+}
+*/
